6f42b8e76e1f234a54eb8cddd91a19beb161fa86
qvv214 <qvv214@gmail.com>
2021-10-04 20:42:57 +0300

    fix

--- core/src/main/java/ru/digitalleague/core/controller/Controller.java
+++ core/src/main/java/ru/digitalleague/core/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.digitalleague.core.api.TaxiService;
+import ru.digitalleague.core.mapper.TaxiInfoMapper;
 import ru.digitalleague.core.model.OrderDetails;
 
 @Slf4j
